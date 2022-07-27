477c69f97a84adc704cecb77fb29444fee153470
Iurii Krasnoshchok <hello@iurii.net>
2022-07-27 18:51:35 +0200

    redesign: optimize for 16:9 screens, remove resume part

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// sticky navbar
-window.addEventListener("scroll", () => {
-    let header = document.getElementById("sticky-header");
-    if (window.pageYOffset > header.offsetHeight * 1.3) {
-        header.style.position = "fixed";
-    } else {
-        header.style.position = "relative";
-    }
-});
